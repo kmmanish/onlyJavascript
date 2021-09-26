0806afb1f9c7d9a798d7603e57af0dd6c8f12877
kmmanish <84233822+kmmanish@users.noreply.github.com>
2021-09-26 10:15:55 -0400

    Create example2.js

---
+++ example2.js
@@ -0,0 +1,27 @@
+// Manish has 15 mangoes, Saurabh has 7 mangoes.
+// Saurabh gives 3 mangoes to Mohit.
+// Manish gives 6 mangoes to Mohit.
+// If manish and Mohit have same number of mangoes
+// then Mohit gives one mango back to Manish.
+// Find total mangoes each have.
+
+Manish = 15;
+Saurabh = 7;
+Mohit = 0;
+
+Mohit = 3;
+Saurabh = Saurabh - 3;
+Mohit = Mohit + 6;
+Manish = Manish - 6;
+
+if (Manish == Mohit)
+{
+    console.log('Both Manish and Mohit have same mangoes')
+    Mohit = Mohit-1;
+    Manish = Manish+1;
+}
+
+
+console.log(Manish);
+console.log(Saurabh);
+console.log(Mohit);
