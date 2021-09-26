a2f44a2247203983c92a3d2eb2e6962b218c3aed
kmmanish <84233822+kmmanish@users.noreply.github.com>
2021-09-26 10:03:55 -0400

    Update and rename try.js to firstJSFile.js

---
+++ firstJSFile.js
@@ -0,0 +1,42 @@
+// This is a comment line
+
+// We will learn following things today
+// Data types : Integer, Float, Boolean, Character, String
+// = means giving a value
+// == checking equality
+// if condition, if-else condition
+
+
+
+//Integer data type - 98, -100, 0, 35  --- Integers do not have decimal in it
+// Float data type - 98, 98.09, -100, -100.0, -25.45 --- Numbers that can have decimal
+// Boolean data type - true or false
+// Character data type - a, p, r, @, =, 9, 4
+// String data type - ab, hello, opppo, a (1 or more character)
+
+x = 9;  // x is an Integer variable with value 9
+y = 'L'; // y is a character variable with value L
+a = "Hello"; // a is a string variable with value Hello
+r = 67.20; // r is a float variable with value 67.20
+g = true; // g is a boolean variable with value true
+
+p = 10;
+
+
+// If condition
+
+if (x == p)
+{
+    console.log("Values are equal");
+}
+
+// If else condition
+
+if (x == p)
+{
+    console.log(" Values are equal");
+}
+else
+{
+    console.log("values are not equal");
+}
--- try.js
+++
@@ -1,2 +0,0 @@
-
-console.log('Yo! Wassup?');
