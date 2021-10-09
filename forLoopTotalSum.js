dfec66dab200a9916982028ec7abf13e454a4ba2
kmmanish <84233822+kmmanish@users.noreply.github.com>
2021-10-09 09:56:28 -0400

    Create forLoopTotalSum.js

---
+++ forLoopTotalSum.js
@@ -0,0 +1,8 @@
+scores = [6,4,3,7];
+totalSum = 0;
+for (let counter = 0; counter<=3; counter=counter+1)
+{
+    totalSum = totalSum + scores[counter];
+}
+
+console.log("total sum is: ", totalSum);
