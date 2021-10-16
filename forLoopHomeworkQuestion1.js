011d3486f7b6fa59e56be8bb6f3bf277a55d4de6
kmmanish <84233822+kmmanish@users.noreply.github.com>
2021-10-16 09:45:55 -0400

    Create forLoopHomeworkQuestion1.js

---
+++ forLoopHomeworkQuestion1.js
@@ -0,0 +1,8 @@
+//Question 1: Make a list of 5 motorcycle company. In a for loop print all the companies from this list
+
+motorcycle = ["honda","bajaj","tvs","yamaha","hero"];
+
+for (let counter = 0 ; counter <= 4 ; counter = counter + 1)
+{
+    console.log(motorcycle[counter]);
+}
